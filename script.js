22710f26a4f2e1e32f5f6a36146a3ab02e1460f4
hsj0402 <hsjain0402@gmail.com>
2024-07-12 23:35:56 +0530

    ff

--- script.js
+++ script.js
@@ -16,3 +16,43 @@ function opentab(tabname, element) {
     
 }
 
+
+// function([string1, string2],target id,[color1,color2])    
+ function consoleText(words, id, colors) {
+      if (colors === undefined) colors = ['#fff'];
+      let visible = true;
+      const con = document.querySelector('.console-underscore');
+      let letterCount = 0;
+      let waiting = false;
+      const target = document.getElementById(id);
+      let currentWordIndex = 0;
+
+      target.setAttribute('style', 'color:' + colors[currentWordIndex]);
+
+      function typeSentence() {
+        if (waiting) return;
+
+        if (letterCount <= words[currentWordIndex].length) {
+          target.innerHTML = words[currentWordIndex].substring(0, letterCount);
+          letterCount++;
+        } else {
+          waiting = true;
+          setTimeout(() => {
+            waiting = false;
+            letterCount = 0;
+            currentWordIndex = (currentWordIndex + 1) % words.length;
+            target.setAttribute('style', 'color:' + colors[currentWordIndex]);
+            target.innerHTML = '';
+          }, 1000);
+        }
+      }
+
+      setInterval(typeSentence, 120);
+
+      setInterval(function() {
+        con.classList.toggle('hidden');
+        visible = !visible;
+      }, 400);
+    }
+
+    consoleText(['Wordpress Developer', 'MERN Stack Developer', 'Software Developer'], 'console', ['White','White','White']);
