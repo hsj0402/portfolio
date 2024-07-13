c2e9d89d1256df302e5bab1246708d018f6269aa
hsj0402 <hsjain0402@gmail.com>
2024-07-13 17:31:16 +0530

    c

--- script.js
+++ script.js
@@ -18,43 +18,34 @@ function opentab(tabname, element) {
 
 
 // function([string1, string2],target id,[color1,color2])    
- function consoleText(words, id, colors) {
-      if (colors === undefined) colors = ['#fff'];
-      let visible = true;
-      const con = document.querySelector('.console-underscore');
-      let letterCount = 0;
-      let waiting = false;
-      const target = document.getElementById(id);
-      let currentWordIndex = 0;
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  let letterCount = 0;
+  let currentWordIndex = 0;
+  let waiting = false;
+  const target = document.getElementById(id);
 
-      target.setAttribute('style', 'color:' + colors[currentWordIndex]);
+  function typeSentence() {
+    if (waiting) return;
 
-      function typeSentence() {
-        if (waiting) return;
-
-        if (letterCount <= words[currentWordIndex].length) {
-          target.innerHTML = words[currentWordIndex].substring(0, letterCount);
-          letterCount++;
-        } else {
-          waiting = true;
-          setTimeout(() => {
-            waiting = false;
-            letterCount = 0;
-            currentWordIndex = (currentWordIndex + 1) % words.length;
-            target.setAttribute('style', 'color:' + colors[currentWordIndex]);
-            target.innerHTML = '';
-          }, 1000);
-        }
-      }
-
-      setInterval(typeSentence, 120);
-
-      setInterval(function() {
-        con.classList.toggle('hidden');
-        visible = !visible;
-      }, 400);
+    if (letterCount < words[currentWordIndex].length) {
+      target.innerHTML = words[currentWordIndex].substring(0, letterCount + 1);
+      letterCount++;
+    } else {
+      waiting = true;
+      setTimeout(() => {
+        waiting = false;
+        letterCount = 0;
+        currentWordIndex = (currentWordIndex + 1) % words.length;
+        target.innerHTML = '';
+      }, 1000);
     }
+  }
 
-    consoleText(['MERN Stack Developer', 'Wordpress Developer', 'Software Developer'], 'console', ['#148ac2','#1bdfff','#3f00ff']);
+  setInterval(typeSentence, 120);
+}
+
+consoleText(['MERN Stack Developer', 'Wordpress Developer', 'Software Developer'], 'text', ['#ff0000', '#00ff00', '#0000ff']);
+    
 
 
